b7836647bcf47ca0eda02f78cc2c682e12f9f30b
Blake Sorenson <bsorenson@fedora26.localdomain>
2020-04-28 11:31:01 -0500

    Backup 2020/04/28

--- covid_data.py
+++ covid_data.py
@@ -6,4 +6,80 @@
 """
 
 import numpy as np
-from covid_lib import split_metar_file, read_meteo_data,read_covid_data
+import matplotlib.pyplot as plt
+from covid_lib import *
+
+all_locs = True
+if(all_locs == True):
+    for i, key in enumerate(county_dict):
+        if(i==0):
+            meteo_dict = read_meteo_data(key+'_area_temp_wind.csv')
+        else:
+            meteo_dict = read_meteo_data(key+'_area_temp_wind.csv',meteo_dict=meteo_dict)
+            
+## Read NYC and newOrleans meteo data
+#meteo_dict = read_meteo_data('nyc_area_temp_wind.csv')
+#meteo_dict = read_meteo_data('newOrleans_area_temp_wind.csv',meteo_dict=meteo_dict)
+#meteo_dict = read_meteo_data('gfk_area_temp_wind.csv')
+#meteo_dict = read_meteo_data('dto_area_temp_wind.csv',meteo_dict=meteo_dict)
+
+# Read the COVID data
+covid_dict = read_covid_data()
+
+    # The dictionary will have keys:
+    # covid_dict['dates']
+    # covid_dict['temp']
+    # covid_dict['u']
+    # covid_dict['v']
+    # covid_dict['data']
+    #   > This will be a second dictionary holding all the
+    #       counties.
+    #   covid_dict['data']['Queens County NY']
+    #       > Below this dictionary will be all the daily 
+    #         death counts and confirmed case counts
+    #       covid_dict['data']['Queens County NY']['deaths']
+    #       covid_dict['data']['Queens County NY']['cases']
+    #       covid_dict['data']['Queens County NY']['population']
+
+for key in county_dict.keys():
+    # Calculate cases / 100000 for the desired counties
+    covid_key = county_dict[key]
+    covid_dict['data'][covid_key]['cases_p100k'] = 100000*\
+        covid_dict['data'][covid_key]['cases']/covid_dict['data'][covid_key]['population']
+    covid_dict['data'][covid_key]['deaths_p100k'] = 100000*\
+        covid_dict['data'][covid_key]['deaths']/covid_dict['data'][covid_key]['population']
+
+    # Calculate daily cases and deaths
+    daily_cases  = np.copy(covid_dict['data'][covid_key]['cases'])
+    daily_deaths = np.copy(covid_dict['data'][covid_key]['deaths'])
+    daily_cases[0] = 0
+    daily_deaths[0] = 0
+    for i in range(1,len(daily_cases)):
+        daily_cases[i] = covid_dict['data'][covid_key]['cases'][i]-\
+                         covid_dict['data'][covid_key]['cases'][i-1]
+        daily_deaths[i] = covid_dict['data'][covid_key]['deaths'][i]-\
+                         covid_dict['data'][covid_key]['deaths'][i-1]
+    covid_dict['data'][covid_key]['daily_cases'] = daily_cases 
+    covid_dict['data'][covid_key]['daily_deaths'] = daily_deaths 
+
+#county='gfk'
+#mvar = 'avg_temp'
+#cvar = 'cases'
+#plot_single_county(meteo_dict,covid_dict,county,mvar,cvar)
+
+##!#covid_variables = ['cases','deaths','cases_p100k','deaths_p100k','daily_cases','daily_deaths']
+##!#meteo_variables = ['avg_temp','min_temp','max_temp','avg_u','avg_v']
+##!## Make all the images
+##!#for lkey in county_dict.keys():
+##!#    print("Location = ",lkey)
+##!#    covid_key = county_dict[lkey]
+##!#    for covid_var in covid_variables:
+##!#        print("  COVID variable = ",covid_var)
+##!#        plot_counties_covid(covid_dict,covid_var)
+##!#        for meteo_var in meteo_variables:
+##!#            print("  meteo variable = ",meteo_var)
+##!#            plot_counties_meteo(meteo_dict,meteo_var)
+##!#            plot_single_county(meteo_dict,covid_dict,lkey,meteo_var,covid_var)
+##!#            plot_cases_scatter(meteo_dict,covid_dict,lkey,meteo_var,covid_var)
+
+
--- covid_lib.py
+++ covid_lib.py
@@ -14,6 +14,23 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import imageio
 
+county_dict = {
+    'nyc': 'Queens County NY',
+    'newOrleans': 'Orleans Parish LA',
+    'gfk': 'Grand Forks County ND',
+    'denton': 'Denton County TX' ,
+    'lac': 'Los Angeles County CA'
+}
+
+# County areas, in square miles
+area_dict = {
+    'nyc': 108.1,
+    'newOrleans': 349.8,
+    'gfk': 1440.,
+    'denton': 953.,
+    'lac': 4751. 
+}
+
 def split_metar_file(metar_file):
     # This code reads a METAR file and calculates
     # statistics for temp and wind data. For temp,
@@ -58,12 +75,21 @@ def split_metar_file(metar_file):
                     np_us = np.array(us)
                     np_vs = np.array(vs)
 
-                    mean_temp = np.average(np_temps)
-                    min_temp = np.min(np_temps)
-                    max_temp = np.max(np_temps)
+                    if(len(np_temps)==0):
+                        mean_temp=-99.
+                        min_temp=-99.
+                        max_temp=-99.
+                    else:
+                        mean_temp = np.average(np_temps)
+                        min_temp = np.min(np_temps)
+                        max_temp = np.max(np_temps)
 
-                    mean_u = np.average(np_us)
-                    mean_v = np.average(np_vs)
+                    if(len(np_us)==0):
+                        mean_u=-99.
+                        mean_v=-99.
+                    else:
+                        mean_u = np.average(np_us)
+                        mean_v = np.average(np_vs)
 
                     # Print out the data to the files
                     print("Calculated averages: ",old_date)
@@ -115,17 +141,37 @@ def read_meteo_data(meteo_file,meteo_dict=None):
             templine = line.strip().split(',')
             print(templine[1])
             meteo_dict[site_name]['dates'].append(templine[1])
-            meteo_dict[site_name]['avg_temp'][i] = float(templine[2])
-            meteo_dict[site_name]['min_temp'][i] = float(templine[3])
-            meteo_dict[site_name]['max_temp'][i] = float(templine[4])
-            meteo_dict[site_name]['avg_u'][i] = float(templine[5])
-            meteo_dict[site_name]['avg_v'][i] = float(templine[6])
+            # Average temperature
+            if(templine[2]=='-99.0'):
+                meteo_dict[site_name]['avg_temp'][i] = np.nan 
+            else:
+                meteo_dict[site_name]['avg_temp'][i] = float(templine[2])
+            # Min temperature 
+            if(templine[3]=='-99.0'):
+                meteo_dict[site_name]['min_temp'][i] = np.nan
+            else:
+                meteo_dict[site_name]['min_temp'][i] = float(templine[3])
+            # Max temperature
+            if(templine[4]=='-99.0'):
+                meteo_dict[site_name]['max_temp'][i] = np.nan
+            else:
+                meteo_dict[site_name]['max_temp'][i] = float(templine[4])
+            # U wind
+            if(templine[5]=='-99.0'):
+                meteo_dict[site_name]['avg_u'][i] = np.nan
+            else:
+                meteo_dict[site_name]['avg_u'][i] = float(templine[5])
+            # V wind
+            if(templine[6]=='-99.0'):
+                meteo_dict[site_name]['avg_v'][i] = np.nan
+            else:
+                meteo_dict[site_name]['avg_v'][i] = float(templine[6])
 
     return meteo_dict
 # num_mets is the number of days of met observations in the
 # meteo_dict. Assumes that the same start date is used for 
 # the meteo data and the covid data.
-def read_covid_data(num_mets):
+def read_covid_data():
     # Grab the confirmed cases, deaths, and population csv files
     # from the directory.
     case_file = 'covid_confirmed_usafacts.csv'
@@ -137,9 +183,6 @@ def read_covid_data(num_mets):
     covid_dict['data'] = {}
     # The dictionary will have keys:
     # covid_dict['dates']
-    # covid_dict['temp']
-    # covid_dict['u']
-    # covid_dict['v']
     # covid_dict['data']
     #   > This will be a second dictionary holding all the
     #       counties.
@@ -160,9 +203,12 @@ def read_covid_data(num_mets):
         print("Reading case file")
         for i, line in enumerate(clines[1:]):
             templine = line.strip().split(',')
+            if(i==0):
+                covid_dict['dates'] = templine[4:]
             county_key = ' '.join([templine[1],templine[2]])
             covid_dict['data'][county_key] = {}
-            covid_dict['data'][county_key]['cases'] = templine[start_ind:start_ind+num_mets]
+            covid_dict['data'][county_key]['cases'] = np.array([int(num) for num in templine[start_ind:]])
+            # Calculate cases per 
     # Read the death data
     with open(death_file,'r') as dfile:
         dlines = dfile.readlines()
@@ -172,11 +218,11 @@ def read_covid_data(num_mets):
             templine = line.strip().split(',')
             county_key = ' '.join([templine[1],templine[2]])
             try:
-                covid_dict['data'][county_key]['deaths'] = templine[start_ind:start_ind+num_mets]
+                covid_dict['data'][county_key]['deaths'] = np.array([int(num) for num in templine[start_ind:]])
             except:
                 county_key = ' '.join([templine[1],'and','City',templine[2]])
                 if(county_key in covid_dict['data'].keys()):
-                    covid_dict['data'][county_key]['deaths'] = templine[start_ind:start_ind+num_mets]
+                    covid_dict['data'][county_key]['deaths'] = np.array([int(num) for num in templine[start_ind:]])
 
     # Read the population data
     with open(pop_file,'r') as pfile:
@@ -187,186 +233,131 @@ def read_covid_data(num_mets):
             templine = line.strip().split(',')
             county_key = ' '.join([templine[1],templine[2]])
             try:
-                covid_dict['data'][county_key]['population'] = templine[3]
+                covid_dict['data'][county_key]['population'] = int(templine[3])
             except:
                 county_key = ' '.join([templine[1],'and','City',templine[2]])
                 if(county_key in covid_dict['data'].keys()):
-                    covid_dict['data'][county_key]['population'] = templine[3]
+                    covid_dict['data'][county_key]['population'] = int(templine[3])
 
     return covid_dict
-def get_daily_data(case_str,var='olr'):
-    latmax = 180  # 19.5 N
-    latmin = 0   # 19.5 S
 
-    if(var=='olr'):
-        file_var = 'toa_lw_all_daily'
-        label = 'OLR Anomaly [W/m2]'
-        anom_max = 60
-        anom_min = -60
-    elif(var=='press'):
-        file_var = 'aux_surf_press_daily'
-        label = 'SLP Anomaly [hPa]'
-        anom_max = 5 
-        anom_min = -5
-    else:
-        print("Invalid variable option. Either 'olr' or 'press'")
-        return
+def plot_single_county(meteo_dict,covid_dict,county,meteo_var,covid_var,save=False):
 
-    # Path to data files
-    data_path = '/data/CERES/SSF_1Deg/daily/Terra/'
+    meteo_key = county
+    # Use the user-defined county variable to grab the county
+    # key for the covid dict
+    county_key = county_dict[county]
 
-    if(case_str=='case1'):
-        # Grab the 2000 - 2001 file
-        filename = 'CERES_SSF1deg-Day_Terra-MODIS_Ed4A_Subset_20000601-20010630.nc'
-    elif(case_str=='case2'):
-        # Grab the 2000 - 2001 file
-        filename = 'CERES_SSF1deg-Day_Terra-MODIS_Ed4A_Subset_20170501-20180531.nc'
-    else:
-        print("Invalid case. Try again")
-        return  
-
-    # Grab the daily data
-    daily_dict = {}
-    data = Dataset(data_path+filename,"r")
-    #data = Dataset(data_path+"CERES_SSF1deg-Day_Terra-MODIS_Ed4A_Subset_20171101-20180228.nc","r")
-
-    # Grab the times and create datetime objects
-    start_date = datetime.date(2000,3,1)
-    dates = [start_date + datetime.timedelta(days= \
-            int(data.variables['time'][ti].data))  \
-            for ti in range(data.dimensions['time'].size)]
-
-    daily_dict['var'] = var
-    daily_dict['time'] = data.variables['time'][:]
-    daily_dict['lat']  = data.variables['lat'][latmin:latmax]
-    daily_dict['lon']  = data.variables['lon'][:]
-    print("Accessing dictionary with file_var = ",file_var)
-    daily_dict['daily_vals'] = data.variables[file_var][:,latmin:latmax,:]
-    daily_dict['dates'] = dates
-    daily_dict['anom_max'] = anom_max
-    daily_dict['anom_min'] = anom_min
-    daily_dict['label'] = label
+    # Plot stuff
+    fig, ax1 = plt.subplots()
+    plt.title(county_key)
+    color='tab:red'
+    ax1.plot(meteo_dict[meteo_key][meteo_var],color=color,label='meteo_var')
+    ax1.set_ylabel(meteo_var.title(),color=color)
     
-    return daily_dict
-
-def plot_anomaly(daily_dict,climo_dict,tidx):
-    # Calculate anomalies
-    anomalies = daily_dict['daily_vals'][tidx,:,:] - climo_dict['climo'][:,:]
-
-    data_proj = ccrs.PlateCarree()
-    plot_proj = ccrs.Miller()
-    colormap = plt.cm.bwr
-   
-    fig1 = plt.figure() 
-    ax = plt.subplot(projection = plot_proj)
-    ax.coastlines()
-    ax.set_extent((40,170,-40,40),crs=data_proj)
-    ax.set_yticks([-40,-30,-20,-10,0,10,20,30,40],crs=data_proj)
-    ax.set_xticks([40,60,80,100,120,140,160],crs=data_proj)
-    lon_formatter = LongitudeFormatter(zero_direction_label=True)
-    lat_formatter = LatitudeFormatter()
-    ax.xaxis.set_major_formatter(lon_formatter)
-    ax.yaxis.set_major_formatter(lat_formatter)
-    mesh = plt.pcolormesh(daily_dict['lon'], daily_dict['lat'], \
-             anomalies, transform=data_proj,cmap=colormap, \
-             vmin=daily_dict['anom_min'],vmax=daily_dict['anom_max'])
-    cbar = plt.colorbar(mesh,shrink=0.62,label= daily_dict['label'])
-    ax.set_title(datetime.datetime.strftime(daily_dict['dates'][tidx],'%Y-%m-%d'))
-    outname = 'mjo_'+daily_dict['var']+'_anom_'+datetime.datetime.strftime(daily_dict['dates'][tidx],'%Y%m%d')+'.png'
-    print("Saving image ",outname)
-    plt.savefig(outname,dpi=100)    
-    plt.close('all')
-
-def make_gif(var):
-    # Grab all the image files
-    filenames = check_output('ls mjo_'+var+'_anom_*.png',shell=True).decode('utf-8').strip().split('\n')
-    images = []
-    filedate = filenames[0][13:19]
-    filedate_end = filenames[-1][13:19]
-    for filename in filenames:
-        print(filename)
-        images.append(imageio.imread(filename))
-    gifname = 'mjo_'+var+'_loop_'+filedate+'_'+filedate_end+'.gif'
-    imageio.mimsave(gifname,images)
-    print("Saved gif "+gifname)
-
-def plot_lon_time(daily_dict,climo_dict):
-
-    # Make array to hold lat averaged values
-    latmin = 80   # 10 S
-    latmax = 100  # 10 N
-    lonmin = 0 
-    lonmax = 360
-
-    longitudes = daily_dict['lon'][lonmin:lonmax]
-    date_ranges = np.arange(len(daily_dict['dates']))
-    lat_avgs = np.zeros((daily_dict['daily_vals'].shape[0],lonmax-lonmin))
-
-    for ti in range(daily_dict['daily_vals'].shape[0]):
-        print(daily_dict['dates'][ti])
-        # Calculate anomalies
-        anomalies = daily_dict['daily_vals'][ti,:,:] - climo_dict['climo'][:,:]
-
-        # Calculate lat average
-        lat_avg = np.ma.average(anomalies[latmin:latmax,:].data,axis=0)
-
-        # Insert the desired lat averages over the desired longitude
-        # range into the average array
-        lat_avgs[ti,:] = lat_avg[lonmin:lonmax]
-
-    if(daily_dict['daily_vals'].shape[0]>180):
-        figsize=(5,11)
-        label_int = 32
-        format_str = '%b %Y'
+    ax2 = ax1.twinx()
+    
+    color='tab:blue'
+    ax2.plot(covid_dict['data'][county_key][covid_var],color=color,label='Cases')
+    ax2.set_ylabel('# '+covid_var.title(),color=color)
+    
+    image_name = county+'_'+meteo_var+'_'+covid_var+'_single_county.png'
+    if(save==True):
+        plt.savefig(image_name,dpi=300)
+        print("Saved image",image_name)
     else:
-        figsize=(8,7)
-        label_int = 14
-        format_str = '%Y-%m-%d'
-    # Get the dates to put in list
-    # Go every 2 weeks
-    label_text = [datetime.datetime.strftime(dx,format_str) \
-                 for dx in daily_dict['dates'][::label_int]]
-    myFmt = mdates.DateFormatter(format_str)
-    plevels = np.arange(daily_dict['anom_min'],daily_dict['anom_max'])
-    #plevels = np.linspace(-62,60,100)
-    lat_avgs[lat_avgs < daily_dict['anom_min']] = daily_dict['anom_min']
-    # Make the figure
-    fig1 = plt.figure(figsize=figsize)
-    plt.contourf(longitudes,date_ranges,lat_avgs,\
-                 cmap = plt.cm.bwr,levels=plevels,\
-                 extend='min')
-    plt.colorbar(label=daily_dict['label'])
-    plt.yticks(np.arange(daily_dict['daily_vals'].shape[0])[::label_int],\
-               label_text,fontsize=8)
-    plt.xlabel('Longitude')
-    plt.xticks(np.arange(len(daily_dict['lon']))[::60],\
-               daily_dict['lon'][::60],fontsize=8)
-#    plt.ylabel('Days after 2017/11/01')
-    outname = 'mjo_lon_time_'+daily_dict['var']+'_'+datetime.datetime.strftime(daily_dict['dates'][0],'%Y%m%d')+'.png'
-    plt.savefig(outname,dpi=300)
-    print("Saved image "+outname)
-    plt.show()
-    ##data_proj = ccrs.PlateCarree()
-    ##plot_proj = ccrs.Miller()
-    ##colormap = plt.cm.bwr
-   
-    ##fig1 = plt.figure() 
-    ##ax = plt.subplot(projection = plot_proj)
-    ##ax.coastlines()
-    ##ax.set_extent((40,170,-40,40),crs=data_proj)
-    ##ax.set_yticks([-40,-30,-20,-10,0,10,20,30,40],crs=data_proj)
-    ##ax.set_xticks([40,60,80,100,120,140,160],crs=data_proj)
-    ##lon_formatter = LongitudeFormatter(zero_direction_label=True)
-    ##lat_formatter = LatitudeFormatter()
-    ##ax.xaxis.set_major_formatter(lon_formatter)
-    ##ax.yaxis.set_major_formatter(lat_formatter)
-    ##mesh = plt.pcolormesh(daily_dict['lon'], daily_dict['lat'], \
-    ##         anomalies, transform=data_proj,cmap=colormap, \
-    ##         vmin=-150,vmax=150)
-    ##cbar = plt.colorbar(mesh,shrink=0.62,label= 'OLR Anomaly [W/m2]')
-    ##ax.set_title(datetime.datetime.strftime(daily_dict['dates'][tidx],'%Y-%m-%d'))
-    ##outname = 'mjo_olr_anom_'+datetime.datetime.strftime(daily_dict['dates'][tidx],'%y%m%d')+'.png'
-    ##print("Saving image ",outname)
-    ##plt.savefig(outname,dpi=100)    
-    ##plt.close('all')
+        plt.show()
+    plt.close()
 
+def plot_counties_covid(covid_dict,covid_var,save=False):
+    fig1, ax1 = plt.subplots()
+    ax1.set_title(covid_var.title())
+    for key in county_dict.keys():
+        ax1.plot(covid_dict['data'][county_dict[key]][covid_var],label=county_dict[key])
+    ax1.set_ylabel(covid_var.title())
+    ax1.set_xlabel('Days after 2020-01-22')
+    plt.legend()
+
+    image_name = covid_var+'_all_counties.png'
+    if(save==True):
+        plt.savefig(image_name,dpi=300)
+        print("Saved image",image_name)
+    else:
+        plt.show()
+    plt.close()
+
+def plot_counties_meteo(meteo_dict,meteo_var,save=False):
+    fig1, ax1 = plt.subplots()
+    ax1.set_title(meteo_var.title())
+    for key in meteo_dict.keys():
+        #print(key)
+        ax1.plot(meteo_dict[key][meteo_var],label=key)
+    ax1.set_ylabel(meteo_var.title())
+    ax1.set_xlabel('Days after 2020-01-22')
+    plt.legend()
+
+    image_name = meteo_var+'_all_counties.png'
+    if(save==True):
+        plt.savefig(image_name,dpi=300)
+        print("Saved image",image_name)
+    else:
+        plt.show()
+    plt.close()
+
+
+# Plot scatter plots of avg temp vs cases and avg u and v vs cases
+def plot_cases_scatter(meteo_dict,covid_dict,county,meteo_var,covid_var,save=False):
+
+    # Assume that the starting date for both the COVID and 
+    # meteorology data are the same, so use the smaller of the 
+    # two to determine the size
+
+    # Meteorology data are on the x axis
+    x1_vals = np.copy(meteo_dict[county][meteo_var])
+    #x2_vals = np.copy(meteo_dict[county]['avg_u'])
+    #x3_vals = np.copy(meteo_dict[county]['avg_v'])
+
+    # COVID data are on the y axis
+    y_vals = np.copy(covid_dict['data'][county_dict[county]][covid_var])
+
+    if(len(x1_vals)>len(y_vals)):
+        x1_vals = x1_vals[:len(y_vals)]
+        #x2_vals = x2_vals[:len(y_vals)]
+        #x3_vals = x3_vals[:len(y_vals)]
+    elif(len(x1_vals)<len(y_vals)):
+        y_vals = y_vals[:len(x1_vals)]
+
+    # Eliminate data where case or death counts are zero
+    x1_vals = x1_vals[y_vals!=0] 
+    y_vals = y_vals[y_vals!=0] 
+
+    fig1, axs = plt.subplots()
+    #fig1, axs = plt.subplots(3)
+    markersize=8
+    # Plot avg temp
+    axs.scatter(x1_vals,y_vals,s=markersize)
+    axs.set_title(county_dict[county]+' '+meteo_var.title())
+    axs.set_ylabel(covid_var.title())
+    axs.set_xlabel(meteo_var.title())
+
+    ### Plot avg zonal wind
+    ##axs[1].scatter(x2_vals,y_vals,s=markersize)
+    ##axs[1].set_title('Avg Zonal Wind')
+    ##axs[1].set_ylabel(covid_var.title())
+    ##axs[1].set_xlabel('Avg Zonal Wind [m/s]')
+
+    ### Plot avg zonal wind
+    ##axs[2].scatter(x3_vals,y_vals,s=markersize)
+    ##axs[2].set_title('Avg Meridional Wind')
+    ##axs[2].set_ylabel(covid_var.title())
+    ##axs[2].set_xlabel('Avg Meridional Wind [m/s]')
+    if(save==True):
+        image_name = county+'_'+meteo_var+'_'+covid_var+'_scatter.png'
+        plt.savefig(image_name,dpi=300)
+        print("Saved image",image_name)
+    else:
+        plt.show()
+    plt.close()
+
+def plot_pdense_temp_scatter(meteo_dict,covid_dict):
+    print("Here's the dictionary")
